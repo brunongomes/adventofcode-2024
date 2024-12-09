4c1355ce0052f61d5aaabf50dd9247d18b4567d5
Bruno Gomes <brunongomes8@gmail.com>
2024-12-09 13:37:35 -0300

    feat: implement solution for Day 3 - Mull It Over

--- DAY_3.py
+++ DAY_3.py
@@ -1,23 +1,40 @@
 import re
 
-def extract_mul_patterns(text):
-    pattern = r"mul\(\d+,\d+\)"
-    matches = re.findall(pattern, text)
-    return matches
+def extract_patterns(pattern, text):
+    return re.findall(pattern, text)
 
-def sum_of_multiplications(array):
-    pattern = r"\d+"
-    values = re.findall(pattern, array)
-    multiplication = int(values[0]) * int(values[1])
-    return multiplication
+def calculate_multiplication(expression):
+    numbers = extract_patterns(r"\d+", expression)
+    return int(numbers[0]) * int(numbers[1])
 
-with open('input.txt', 'r') as file:
-    merged_array = []
-    for line in file:
-        instructions = extract_mul_patterns(line)
-        merged_array += instructions
-    sum_of_results = 0
-    for instruction in merged_array:
-        result = sum_of_multiplications(instruction)
-        sum_of_results += result
-    print(f'add up all of the results: {sum_of_results}')
+def process_file(file_path):
+    instructions = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            instructions.extend(extract_patterns(r"do\(\)|don't\(\)|mul\(\d+,\d+\)", line))
+    return instructions
+
+def sum_of_multiplications(file_path):
+    instructions = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            instructions.extend(extract_patterns(r"mul\(\d+,\d+\)", line))
+    total = sum(calculate_multiplication(instruction) for instruction in instructions)
+    print(f'add up all of the results: {total}')
+
+def do_dont_logic(file_path):
+    instructions = process_file(file_path)
+    status = True
+    total = 0
+    for instruction in instructions:
+        if instruction == "do()":
+            status = True
+        elif instruction == "don't()":
+            status = False
+        elif "mul" in instruction and status:
+            total += calculate_multiplication(instruction)
+    print(f'add up all of the results with do and dont: {total}')
+
+file_path = 'input.txt'
+sum_of_multiplications(file_path)
+do_dont_logic(file_path)
